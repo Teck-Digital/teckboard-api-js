6466fba4641b67f9189ad78077a8a79d7b06d016
Yannik Hahn <yannihahn@t-online.de>
2022-01-04 21:27:40 +0100

    Adds auth package, reverts pagination response

--- packages/v1/endpoint/pagination/PaginationResponse.ts
+++ packages/v1/endpoint/pagination/PaginationResponse.ts
@@ -1,20 +1,15 @@
-import { AxiosResponse } from 'axios';
-
 export default interface PaginationResponse<T> {
-    response: AxiosResponse<T>;
-    data: {
-        chunk: number;
-        chunk_size: number;
-        last_chunk: number;
-        urls: {
-            next: string;
-            prev: string;
-            first: string;
-            last: string;
-        };
-        path: string;
-        from: number;
-        to: number;
-        data: T[];
+    chunk: number;
+    chunk_size: number;
+    last_chunk: number;
+    urls: {
+        next: string;
+        prev: string;
+        first: string;
+        last: string;
     };
+    path: string;
+    from: number;
+    to: number;
+    data: T[];
 }
